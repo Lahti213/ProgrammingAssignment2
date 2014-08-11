b53fed26306197c63a2ca09d0c0b4d9695c80faa
Lahti213 <Lahti213@gmail.com>
2014-08-10 21:49:57 -0500

    Adding cacheMatrix.R after completing assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions will cache the inverse of a matrix that the 
+## user may need to use again rather than recomputing the inverse.
 
-## Write a short comment describing this function
+## This function takes the input of a matrix and creates a "special" 
+## matrix. The "special" matrix is simply a list containing four functions.
 
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the "special" vector created with
+## the above functions. However, first it checks if the inverse has already
+## been calculated. If it has, it pulls the mean from cache, skipping the
+## computation. Otherwise, it calculates the inverse and sets the value of
+## the inverse in the cache by way of the setmean function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
